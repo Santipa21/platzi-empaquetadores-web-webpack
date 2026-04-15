72c47031ec24de9d5c4adac6ce6d2b60c44b4969
Santi Patiño <santiph2000@gmail.com>
2026-04-15 11:55:00 -0500

    Webpack project

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { resolve } = require('dns');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: ['.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+    plugins: [
+      new HtmlWebpackPlugin({
+        inject: true,
+        template: './public/index.html',
+        filename: 'index.html'
+      })
+    ]
+
+};
